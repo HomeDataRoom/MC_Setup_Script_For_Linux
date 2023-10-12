6fbc702dee80a5f273d6629110e256e05efbec14
NextPancake401 <121368947+NextPancake401@users.noreply.github.com>
2023-10-12 02:32:27 -0400

    Update MinecraftRconSetup.sh

--- scripts/MinecraftRconSetup.sh
+++ scripts/MinecraftRconSetup.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-read -p "type a password for your rcon console " rpass
-
 sudo git clone https://github.com/Rauks/Minecraft-RCON.git
 
 cd Minecraft-RCON
 
 sudo cp -r * /var/www/html
 
-sudo sed -i "s/localhost/127.0.0.1/g" /var/www/html/config.php
-sudo sed -i "s/xtMJsVtmx0XypuId7jIb/$rpass/g" /var/www/html/config.php
-sudo sed -i "s/rcon.password=/rcon.password=$rpass/g" /opt/minecraft/server.properties
-sudo sed -i "s/enable-rcon=false/enable-rcon=true/g" /opt/minecraft/server.properties
+read -p "Type a password for your rcon console (something strong) " rpass
 
-sudo systemctl stop minecraft.service
-sudo systemctl reload apache2.service
-sudo systemctl start minecraft.service
+sudo sed -i "s/rcon.password=/rcon.password=$rpass/g" /opt/minecraft/server.properties
+sudo sed -i "s/xtMJsVtmx0XypuId7jIb/$rpass/g" /var/www/html/config.php
+sudo sed -i "s/localhost/127.0.0.1/g" /var/www/html/config.php
+sudo sed -i "s/enable-rcon=false/enable-rcon=true/g" /opt/minecraft/server.properties
