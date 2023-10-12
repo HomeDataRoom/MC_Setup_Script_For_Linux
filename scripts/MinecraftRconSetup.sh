3a8070d7b67b8bf3293570f8db7b7a3a074114c9
NextPancake401 <121368947+NextPancake401@users.noreply.github.com>
2023-10-12 00:31:16 -0400

    Update MinecraftRconSetup.sh

--- scripts/MinecraftRconSetup.sh
+++ scripts/MinecraftRconSetup.sh
@@ -11,13 +11,8 @@ sudo cp -r * /var/www/html
 sudo sed -i "s/localhost/127.0.0.1/g" /var/www/html/config.php
 sudo sed -i "s/xtMJsVtmx0XypuId7jIb/$rpass/g" /var/www/html/config.php
 
-sudo su minecraft <<EOF
-bash
-sed -i "s/rcon.password=/rcon.password=$rpass/g" /opt/minecraft/server.properties
-sed -i "s/enable-rcon=false/enable-rcon=true/g" /opt/minecraft/server.properties
-exit
-exit
-EOF
+sudo sed -i "s/rcon.password=/rcon.password=$rpass/g" /opt/minecraft/server.properties
+sudo sed -i "s/enable-rcon=false/enable-rcon=true/g" /opt/minecraft/server.properties
 
 sudo systemctl stop minecraft.service
 sudo systemctl reload apache2.service
